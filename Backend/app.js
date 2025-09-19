88f5f01fd3c8256788ba10db55a9e67b5eb26946
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2025-09-19 20:01:12 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,9 +6,21 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',          
+  'https://video-proctring.vercel.app'  
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",  
-  credentials: true                 
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 app.use(express.json());
 app.use("/api", logRoutes);
